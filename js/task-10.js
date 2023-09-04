efb532111ce3d3b3e42d4e6524b6b11d33af640d
ArturDatsko <133085603+ArturDatsko@users.noreply.github.com>
2023-09-04 23:42:42 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,40 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
-const btnCreate = document.querySelector("button[data-create]");
 
-btnCreate.addEventListener("click", () => {
-  const input = document.querySelector("input");
-  let max = Number(input.max);
-  let min = Number(input.min);
-  let step = Number(input.step);
-  let width = 30;
-  let height = 30;
+const input = document.querySelector("#controls input");
+const buttonCreate = document.querySelector("[data-create]");
+const buttonDestroy = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
 
-  function createBoxes(amount) {
-    if (amount >= min && amount <= max) {
-      for (let i = 1; i <= amount; i += step) {
-        const boxesParent = document.querySelector("#boxes");
-        const boxesChield = document.createElement("div");
-        boxesChield.style.width = `${(width += 10)}px`;
-        boxesChield.style.height = `${(height += 10)}px`;
-        boxesChield.classList.add("boxes-chield");
-        boxesChield.style.backgroundColor = getRandomHexColor();
-        boxesParent.append(boxesChield);
-      }
-    } else {
-      return;
-    }
+function createBoxes() {
+  const amount = Number(input.value);
+  let size = 30;
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
+    let item = document.createElement("div");
+    item.style.width = size + "px";
+    item.style.height = size + "px";
+    item.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(item);
+    size += 10;
   }
-  createBoxes(input.value);
-});
 
-const btnDestroy = document.querySelector("button[data-destroy]");
-btnDestroy.addEventListener("click", () => {
-  const boxesParent = document.querySelector("#boxes"),
-    input = document.querySelector("input");
-  boxesParent.innerHTML = "";
-  input.value = "";
-});
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+buttonCreate.addEventListener("click", createBoxes);
+buttonDestroy.addEventListener("click", destroyBoxes);
