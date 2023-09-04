22c284f5817098eacad7952f995ae6b164e9a8b3
ArturDatsko <133085603+ArturDatsko@users.noreply.github.com>
2023-09-04 08:04:33 +0300

    home-work-6

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const markup = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = ingredient;
+  return li;
+});
+
+const list = document.getElementById("ingredients");
+list.append(...markup);
---
+++ js/task-10.js
@@ -0,0 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const input = document.querySelector(`#controls input`);
+const buttonCreate = document.querySelector(`[data-create]`);
+const buttonDestroy = document.querySelector(`[data-destroy]`);
+const boxes = document.querySelector(`#boxes`);
+
+function createBoxes() {
+  let size = boxes.lastChild ? boxes.lastChild.clientWidth + 10 : 30;
+  let arr = [];
+  for (let i = 1; i <= input.value; i++) {
+    let item = document.createElement(`div`);
+    item.style.width = size + "px";
+    item.style.height = size + "px";
+    size += 10;
+    item.style.backgroundColor = getRandomHexColor();
+    arr.push(item);
+  }
+  boxes.append(...arr);
+}
+
+function destroyBoxes() {
+  [...boxes.children].forEach((box) => box.remove());
+}
+
+buttonCreate.addEventListener(`click`, createBoxes);
+buttonDestroy.addEventListener(`click`, destroyBoxes);
